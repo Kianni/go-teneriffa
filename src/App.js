473f2b415bcf270202d8cfa68af57e96f62cf05b
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2022-02-07 13:28:13 +0200

    remove all unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const DATA = [
+    {
+      id: "",
+      time: { departure: "", arrival: "" },
+      location: { departure_point: "", arrival_point: "" },
+      vehicle: "",
+      additional_info: "",
+    },
+  ];
+  return <div></div>;
 }
 
 export default App;
