2054308971f1a235efac4f023b3b8deb969cbcdc
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2022-02-08 19:59:42 +0200

    add default data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import NestedList from "./components/NestedList";
 import { Transfer } from "./components/Transfer";
-import DATA from "./data";
 
-function App() {
+const defaultData = [
+  {
+    date: "17.6",
+    id: "1000",
+    time: { departure: "8.30", arrival: "11.00" },
+    location: {
+      departure_point: "Stockholm",
+      arrival_point: "Praga",
+    },
+    vehicle: "plane",
+    additional_info: "Lufthansa",
+  },
+];
+
+function App({ travelData = defaultData }) {
   return (
     <>
       <h1>План путешествия</h1>
-      <NestedList data={DATA} />
+      <NestedList data={travelData} />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import DATA from "./data";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App travelData={DATA} />, document.getElementById("root"));
