110fc05775a9cbb7df814419826c72e93aac7626
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2022-02-11 08:38:10 +0200

    deploy without importing data file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import DATA from "./data";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import DATA from "./data";
 
-ReactDOM.render(<App travelData={DATA} />, document.getElementById("root"));
+ReactDOM.render(<App travelData={DATA} />, document.getElementById('root'));
